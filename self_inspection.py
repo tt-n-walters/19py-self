09df5c2005d138f37aee8da44490f412c4cf80a4
nw <nico.walters@techtalents.es>
2020-05-09 17:35:39 +0200

    Almost done

--- self_inspection.py
+++ self_inspection.py
@@ -1,21 +1,30 @@
 
 class Restaurant:
-    def __init__(self, t, num_of_staff):
-        self.tables = t
-        self.staff = num_of_staff
-        self.is_open = False
-    
-
-    def open():
+    def open(self):
         Restaurant.is_open = True
         print("The restaurant is now open!")
+        
+    def __init__(storage, t, num_of_staff):
+        storage.tables = t
+        storage.staff = num_of_staff
+        storage.is_open = False
+
 
 
 
 madrid_location = Restaurant(15, 6)
+
+print(dir(madrid_location))
+# Restaurant.__init__(madrid_location, 15, 6)
+
+
+
+
 london_location = Restaurant(40, 18)
 
-print(madrid_location["tables"])
-print(london_location["tables"])
+madrid_location.open()
+london_location.open()
+
+
 
 
