a946188dfe40d33d40d8f6d9cf4cf6f0235f1dcc
nw <nico.walters@techtalents.es>
2020-05-09 17:10:31 +0200

    Class storage as a dictionary

--- self_inspection.py
+++ self_inspection.py
@@ -1,8 +1,22 @@
 
-class A:
-    def b(self):
-        print("Hello")
+class Restaurant:
+    def create_new():
+        storage = {}
+        storage["tables"] = 10
+        storage["staff"] = 6
+        storage["is_open"] = False
+        return storage
+    
+    
+    def open():
+        Restaurant.is_open = True
+        print("The restaurant is now open!")
 
 
-a = A()
-a.b()
+
+madrid_location = Restaurant.create_new()
+london_location = Restaurant.create_new()
+
+print(madrid_location["tables"])
+
+
