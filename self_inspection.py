7eee75a028e69254e2730b2de33d1067616bf419
nw <nico.walters@techtalents.es>
2020-05-09 17:22:17 +0200

    Class with self

--- self_inspection.py
+++ self_inspection.py
@@ -1,22 +1,21 @@
 
 class Restaurant:
-    def create_new():
-        storage = {}
-        storage["tables"] = 10
-        storage["staff"] = 6
-        storage["is_open"] = False
-        return storage
+    def __init__(self, t, num_of_staff):
+        self.tables = t
+        self.staff = num_of_staff
+        self.is_open = False
     
-    
+
     def open():
         Restaurant.is_open = True
         print("The restaurant is now open!")
 
 
 
-madrid_location = Restaurant.create_new()
-london_location = Restaurant.create_new()
+madrid_location = Restaurant(15, 6)
+london_location = Restaurant(40, 18)
 
 print(madrid_location["tables"])
+print(london_location["tables"])
 
 
